88faa7067b4a9e8504b6dd2e3c0e2475143b909a
DoubleSheeps <568462483@qq.com>
2023-07-01 14:29:53 +0800

    在项目启动时检测Redis是否连接成功

--- src/main/java/com/example/shirodemo/ShiroDemoApplication.java
+++ src/main/java/com/example/shirodemo/ShiroDemoApplication.java
@@ -1,6 +1,13 @@
 package com.example.shirodemo;
 
+import com.example.shirodemo.Utils.JedisUtil;
+import com.example.shirodemo.Utils.JwtUtil;
+import com.example.shirodemo.Utils.StringUtil;
+import com.example.shirodemo.error.BusinessException;
+import com.example.shirodemo.error.EmBusinessError;
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 @MapperScan("com.example.shirodemo.dao")
 public class ShiroDemoApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(ShiroDemoApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(ShiroDemoApplication.class, args);
+        String key = String.valueOf(System.currentTimeMillis());
+        JedisUtil.setJson(key,"true");
+        if(StringUtil.isBlank(JedisUtil.getJson(key))){
+            logger.error("Redis连接失败！");
+        }else {
+            logger.info("Redis连接成功！");
+        }
     }
 
 }
